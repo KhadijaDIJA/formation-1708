6a0b56e7795df2b63ae8cfa96e6662d97cca6d61
khaba <khadija.abahous@smile-maroc.com>
2020-08-19 12:10:20 -0700

    adding test to contact form

---
+++ tests/ContactControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ContactControllerTest extends WebTestCase
+{
+    public function testWeCanContact()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/contact');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists("#contact_email");
+        $this->assertSelectorExists("#contact_message");
+        $client->submitForm('Contacter', [
+            'contact[email]' => 'test@smile.fr',
+            'contact[message]' => 'test',
+            ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextContains('div.alert-success', 'Merci d\'avoir pris contact');
+    }
+
+    /**
+    * Sad path / Unhappy path
+    */
+    public function testTheContactFormDisplaysErrors()
+    {
+
+    }
+
+}
